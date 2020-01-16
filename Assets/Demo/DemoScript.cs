e212638fc2e4f59ac41fea8c55d7ef548a3ab54c
Lixxx15 <55728137+Lixxx15@users.noreply.github.com>
2020-01-16 10:37:22 +0800

    Add files via upload

--- Assets/Demo/DemoScript.cs
+++ Assets/Demo/DemoScript.cs
@@ -7,12 +7,15 @@ public class DemoScript : MonoBehaviour
 {
     public SwitchUI _SwitchUI;
     public DateDropdown _DateDropdown;
+    public PopButton _PopButton;
     public Text _SwitchValue;
     public Text _DateDropdownValue;
+    public Text _PopButtonValue;
     // Start is called before the first frame update
     void Start()
     {
         _SwitchUI.OnValueChange.AddListener((arg) => { _SwitchValue.text = arg.ToString(); });
         _DateDropdown.OnDropdownChange.AddListener((year, month, day) => { _DateDropdownValue.text = year + "/" + month + "/" + day; });
+        _PopButton.OnOptionClick.AddListener((index) => { _PopButtonValue.text = _PopButton._OptionList[index].text; });
     }
 }
